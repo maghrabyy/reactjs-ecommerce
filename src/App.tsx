c3ab8c74eb5162bd5de0fcdce80d90c4bff88c97
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-04-03 19:59:38 +0200

    updating favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import './App.css';
 import constructionImg from './assets/construction-site.png';
+import ecommerceImg from './assets/shopping.png';
 
 function App() {
   return (
     <div className="App h-screen flex flex-col items-center justify-center gap-2 bg-[#1B1B1D]">
+       <img src={ecommerceImg} width={200} className='mb-8' alt="ecommerce platform" />
       <div className="bg-gray-300 py-3 px-4 rounded-md shadow-sm select-none animate-bounce">
         <h1 className='sm:text-6xl text-4xl font-bold text-slate-950 text-center'>eCommerce website under construction.</h1>
       </div>
-      <div className="flex gap-2">
-        <img src={constructionImg} width={200} alt="construction site" />
-        <img src={constructionImg} width={200} alt="construction site" />
-      </div>
+      <img src={constructionImg} width={200} alt="construction site" />
     </div>
   );
 }
